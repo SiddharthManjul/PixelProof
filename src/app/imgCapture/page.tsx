577ec42713ed7ed613a37dfb775721e9aefac147
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-11-30 20:33:33 +0530

    Exif Reader

--- src/app/imgCapture/page.tsx
+++ src/app/imgCapture/page.tsx
@@ -3,7 +3,16 @@
 import { useRef, useState, useCallback } from "react";
 import Webcam from "react-webcam";
 import { RainbowButton } from "@/src/components/ui/rainbow-button";
-// import "./styles.css";
+import EXIF from "exif-js";
+
+interface Metadata {
+  description?: string;
+  dateTime?: string;
+  gpsLocation?: {
+    lat: number;
+    lng: number;
+  } | null;
+}
 
 const videoConstraints = {
   width: 720,
@@ -15,17 +24,40 @@ export default function App() {
   const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
   const webcamRef = useRef<Webcam>(null);
   const [url, setUrl] = useState<string | null>(null);
+  const [metadata, setMetadata] = useState<Metadata | null>(null);
+
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot();
     if (imageSrc) {
       setUrl(imageSrc);
+
+      // Extract metadata from the image
+      const img = new Image();
+      img.src = imageSrc;
+
+      img.onload = function () {
+        EXIF.getData(img as never, () => {
+          const description = EXIF.getTag(this, "ImageDescription");
+          const dateTime = EXIF.getTag(this, "DateTime");
+          const gpsLat = EXIF.getTag(this, "GPSLatitude");
+          const gpsLng = EXIF.getTag(this, "GPSLongitude");
+
+          // Store extracted metadata in state
+          setMetadata({
+            description,
+            dateTime,
+            gpsLocation: gpsLat && gpsLng ? { lat: gpsLat, lng: gpsLng } : null,
+          });;
+        });
+      };
     }
   }, [webcamRef]);
+  console.log(metadata);
 
   return (
     <>
       <header>
-        <h1>Click & Extract!</h1>
+        {/* <h1>Click & Extract!</h1> */}
       </header>
       {isCaptureEnable || (
         <div className="w-full flex flex-col items-center absolute top-36 font-grotesk">
@@ -47,7 +79,7 @@ export default function App() {
               width={540}
               height={360}
               ref={webcamRef}
-              screenshotFormat="image/jpeg"
+              screenshotFormat="image/webp"
               videoConstraints={videoConstraints}
               className="rounded-xl"
             />
@@ -83,6 +115,23 @@ export default function App() {
             </div>
           </div>
         </>
+      )}
+      {metadata && (
+        <div>
+          <h2>Metadata:</h2>
+          <p>
+            <strong>Description:</strong> {metadata.description}
+          </p>
+          <p>
+            <strong>Date & Time:</strong> {metadata.dateTime}
+          </p>
+          {metadata.gpsLocation && (
+            <p>
+              <strong>Location:</strong> Lat: {metadata.gpsLocation.lat}, Lng:{" "}
+              {metadata.gpsLocation.lng}
+            </p>
+          )}
+        </div>
       )}
     </>
   );
